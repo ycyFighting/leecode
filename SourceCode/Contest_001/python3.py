6c385954af0e6717cd551075d8323da42374b734
ycyFighting <100480774+ycyFighting@users.noreply.github.com>
2022-02-28 11:07:10 +0800

    Update python3.py

--- SourceCode/Contest_001/python3.py
+++ SourceCode/Contest_001/python3.py
@@ -1,24 +1,22 @@
-# 暴力枚举
+# 暴力遍历
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-    # 这个:和->是为了说明参数和返回值的数据类型，仅作提示作用
+    def twoSum(self, nums: List[int], target: int) -> List[int]: # :和->仅是对数据类型的解释
         n = len(nums)
-        for i in range(n): # 从第一个数开始找
+        for i in range(n):
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+        
         return []
 
 
-'''
-# 暴力枚举
+# Hash表
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    # 这个:和->是为了说明参数和返回值的数据类型，仅作提示作用
-        n = len(nums)
-        for i in range(n): # 从第一个数开始找
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
-'''
+        hashtable = dict() # dict也是一种{key:value}结构
+        # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标
+        for i, num in enumerate(nums): 
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return [] # 同c++
